e912995520de93111736e8cd017cb35963227f66
ValiTool <perriconelucas@gmail.com>
2023-02-18 20:17:35 -0300

    First build after Spring Init

---
+++ src/main/java/dev/ValiTool/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package dev.ValiTool.movies;
+
+import org.springframework.boot.SpringApplication; //Contains a method called run, in order to run the app.
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ValiTool/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ValiTool.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
